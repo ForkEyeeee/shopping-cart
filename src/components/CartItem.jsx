e4bbc807957e0a08d8dea1bb19dc24e42b189dae
ForkEyeee <shawncarter123456@gmail.com>
2023-08-08 15:31:05 -0400

    styling changes to CartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -4,36 +4,48 @@ const CartItem = ({ image, title, description, price, cartItems, itemId }) => {
   const count = cartItems.filter((cartItem) => cartItem.itemId === itemId)[0]
     .count;
   return (
-    <Box borderWidth="1px" borderRadius="lg" overflow="hidden" m={4}>
-      <Flex>
-        <Box p={4}>
-          <Image
-            borderRadius="md"
-            boxSize="100px"
-            objectFit="cover"
-            src={image}
-            alt={title}
-          />
-          <Text>{count}</Text>
-        </Box>
-        <Box p={4} flex="1">
-          <Flex justify="space-between">
-            <Box>
-              <Heading fontSize="xl">{title}</Heading>
-              <Text mt={2} noOfLines={1}>
-                {description}
-              </Text>
-            </Box>
-            <Box>
-              <Text fontSize="xl">${price}</Text>
-              <Button colorScheme="red" size="sm" mt={2} onClick={() => {}}>
-                Remove
-              </Button>
-            </Box>
-          </Flex>
-        </Box>
-      </Flex>
-    </Box>
+    <Flex
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      m={4}
+      alignItems={"center"}
+      flexDirection={{ base: "column", md: "row" }}
+    >
+      <Box p={4}>
+        <Image
+          borderRadius="md"
+          boxSize="100px"
+          objectFit="cover"
+          src={image}
+          alt={title}
+        />
+        <Text
+          fontSize="sm"
+          fontWeight="medium"
+          p={1}
+          bg="blue.50"
+          borderRadius="sm"
+        >
+          Qty: {count}
+        </Text>
+      </Box>
+      <Box p={4} flex="1">
+        <Flex justify="space-between">
+          <Box textAlign={"center"}>
+            <Heading fontSize="xl">{title}</Heading>
+            <Text mt={2} textAlign={"center"}>
+              {description}
+            </Text>
+            <Text fontSize="xl">${price}</Text>
+            <Button colorScheme="red" size="sm" mt={2} onClick={() => {}}>
+              Remove
+            </Button>
+          </Box>
+          <Box></Box>
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
